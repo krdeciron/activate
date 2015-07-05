2f517ead2dff377b4f7ebff7a6c6f3c0ab54f8a4
EC2 Default User <ec2-user@ip-172-31-28-246.us-west-2.compute.internal>
2015-07-05 11:25:53 +0000

    Migration to amazon ec2

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('bower_components'));
 app.use(session({
     genid: function(req) {
         return uuid.v4();
--- app_http.js
+++ app_http.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.use(function(req, res) {
-   res.redirect('https://urkkdbde4dd3.rdeciron.koding.io' + req.originalUrl);
+   res.redirect('https://ec2-52-27-36-14.us-west-2.compute.amazonaws.com' + req.originalUrl);
 });
 
 module.exports = app
